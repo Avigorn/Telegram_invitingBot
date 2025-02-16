72d46ddbd50e26382dda9ec5b9bd9767e3fa5fca
Alexey <10ulkiorra10@gmail.com>
2025-02-16 19:59:34 +0300

    Add Access Modifiers to base_handler.py

--- handlers/base_handler.py
+++ handlers/base_handler.py
@@ -1,13 +1,31 @@
 from aiogram import Router
-from aiogram.filters import Command, F
-from aiogram.types import Message, CallbackQuery
 
 class BaseHandler:
     def __init__(self, bot, dp):
-        self.bot = bot
-        self.dp = dp
-        self.router = Router()
-        self.dp.include_router(self.router)
+        # Приватные поля (доступны только внутри класса)
+        self.__bot = bot  # Приватное поле для бота <button class="citation-flag" data-index="4">
+        self.__dp = dp    # Приватное поле для диспетчера
 
+        # Защищенное поле (доступно внутри класса и его наследников)
+        self._router = Router()  # Защищенное поле для роутера <button class="citation-flag" data-index="4">
+
+        # Добавляем роутер в диспетчер
+        self.__dp.include_router(self._router)
+
+    # Публичный метод (доступен извне)
     def register_handlers(self):
-        raise NotImplementedError("Метод register_handlers должен быть переопределен в дочернем классе.")
+        """
+        Метод для регистрации обработчиков.
+        Должен быть переопределен в дочерних классах.
+        """
+        raise NotImplementedError("Метод register_handlers должен быть переопределен в дочернем классе.") #<button class="citation-flag" data-index="4">
+
+    # Геттер для приватного поля __bot
+    @property
+    def bot(self):
+        return self.__bot
+
+    # Геттер для приватного поля __dp
+    @property
+    def dp(self):
+        return self.__dp
