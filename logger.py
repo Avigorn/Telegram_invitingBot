bc99b106f470ef04757f67c7f33fa41282e11207
Alexey <10ulkiorra10@gmail.com>
2025-02-22 12:30:58 +0300

    Add a console logging

--- logger.py
+++ logger.py
@@ -6,6 +6,10 @@ def setup_logger():
     logger = logging.getLogger('project_logger')
     logger.setLevel(logging.DEBUG)  # Уровень DEBUG позволит логировать всё
 
+    # Обработчик для вывода в консоль
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)  # Только INFO и выше в консоль
+
     # Обработчик для записи в файл
     file_handler = RotatingFileHandler('app.log', maxBytes=1024*1024*5, backupCount=3)
     file_handler.setLevel(logging.DEBUG)  # Все уровни в файл
@@ -15,6 +19,7 @@ def setup_logger():
     file_handler.setFormatter(formatter)
 
     # Добавление обработчиков к логгеру
+    logger.addHandler(console_handler)
     logger.addHandler(file_handler)
 
     return logger
